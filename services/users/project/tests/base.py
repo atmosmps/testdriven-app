366d92435b1ef059ef5e19515d2b6ba17111f48e
Atmos Maciel <atmos.mps@gmail.com>
2019-05-25 17:54:03 -0300

    refatorando os imports para create_app.

--- services/users/project/tests/base.py
+++ services/users/project/tests/base.py
@@ -1,7 +1,9 @@
 # services/users/project/tests/base.py
 
 from flask_testing import TestCase
-from project import app, db
+from project import create_app, db
+
+app = create_app()
 
 
 class BaseTestCase(TestCase):
