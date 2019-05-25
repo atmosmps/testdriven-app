e69581a2d5528e29c17abf5e07db63fe525a82fd
Atmos Maciel <atmos.mps@gmail.com>
2019-05-25 17:52:55 -0300

    Definindo funcao principal da aplicação, usando o padrão Application Factories do Flask e definindo as chamadas necessarias.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,36 +2,30 @@
 
 import os
 from flask import Flask
-from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-api = Api(app)
-db = SQLAlchemy(app)
-
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+db = SQLAlchemy()
 
 
-# model
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app(script_info=None):
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    # instantiate the app
+    app = Flask(__name__)
 
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-class Userping(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+    # set up extensions
+    db.init_app(app)
 
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
 
-api.add_resource(Userping, '/users/ping/')
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
