adf10473909b89f6ee6d456e58680a916c431cbb
Atmos Maciel <atmos.mps@gmail.com>
2019-05-25 13:12:12 -0300

    Adicionando instancia do SQLALchemy e criação inicial do Model de usuários.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,12 +3,23 @@
 import os
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+db = SQLAlchemy(app)
 
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullabe=False)
+    email = db.Column(db.String(128), nullabe=False)
+    active = db.Column(db.Boolean(), default=True, nullabe=False)
 
 
 class Userping(Resource):
