e45e4e63344048ff0fbff8c7616e8cf8abedcc1b
Atmos Maciel <atmos.mps@gmail.com>
2019-05-25 14:24:53 -0300

    Fazendo correcao em UserMOdel.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -17,9 +17,13 @@ app.config.from_object(app_settings)
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullabe=False)
-    email = db.Column(db.String(128), nullabe=False)
-    active = db.Column(db.Boolean(), default=True, nullabe=False)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class Userping(Resource):
