2cf414580496df1667dba4ce4c15330c2765fe93
Atmos Maciel <atmos.mps@gmail.com>
2019-05-25 16:32:57 -0300

    Add docblocks em uma linha e adicionando SECRET_KET para a aplicação.

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -4,30 +4,23 @@ import os
 
 
 class BaseConfig:
-    """
-    Base Configuration
-    """
+    """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
 
 class DevelopmentConfig(BaseConfig):
-    """
-    Development Configuration
-    """
+    """Development Configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
-    """
-    Testing Configuration
-    """
+    """Testing Configuration"""
     TESTING = True
-    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_TEST_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
-    """
-    Production Configuration
-    """
+    """Production Configuration"""
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
