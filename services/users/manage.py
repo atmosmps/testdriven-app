4314e76ff8808c01a1ccceaffd4bef4a28370549
Atmos Maciel <atmos.mps@gmail.com>
2019-05-25 16:31:00 -0300

    Adicionando função de commando para rodar testes de forma não automatica.

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,5 +1,7 @@
 # services/users/manage.py
 
+import sys
+import unittest
 from flask.cli import FlaskGroup
 from project import app, db
 
@@ -12,6 +14,18 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if result.wasSuccessful():
+        return 0
+
+    sys.exit(result)
 
 
 if __name__ == '__main__':
