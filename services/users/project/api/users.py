5a6fc95245affede3054444aebb09058bc58a433
Atmos Maciel <atmos.mps@gmail.com>
2019-05-25 17:53:44 -0300

    Adicionando estrutura de chamada de end-point neste arquivo.

---
+++ services/users/project/api/users.py
@@ -0,0 +1,18 @@
+# services/users/project/api/users.py
+
+from flask import Blueprint
+from flask_restful import Resource, Api
+
+users_blueprint = Blueprint('users', __name__)
+api = Api(users_blueprint)
+
+
+class UsersPing(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong!'
+        }
+
+
+api.add_resource(UsersPing, '/users/ping/')
