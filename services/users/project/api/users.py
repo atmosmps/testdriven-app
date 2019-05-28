795528bd753cba6e76ec4f262efacf6628073230
Atmos Maciel <atmos.mps@gmail.com>
2019-05-28 11:30:27 -0300

    Adicionando implementação de inserção de usuario.

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,13 +1,16 @@
 # services/users/project/api/users.py
 
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restful import Resource, Api
+from project import db
+from project.api.models import User
 
 users_blueprint = Blueprint('users', __name__)
 api = Api(users_blueprint)
 
 
 class UsersPing(Resource):
+
     def get(self):
         return {
             'status': 'success',
@@ -15,4 +18,22 @@ class UsersPing(Resource):
         }
 
 
+class UsersList(Resource):
+
+    def post(self):
+        post_data = request.get_json()
+        username = post_data.get('username')
+        email = post_data.get('email')
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+
+        response_object = {
+            'status': 'success',
+            'message': f'{email} was added!'
+        }
+
+        return response_object, 201
+
+
+api.add_resource(UsersList, '/users')
 api.add_resource(UsersPing, '/users/ping/')
