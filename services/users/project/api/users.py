93a465b8b237e798d7c5b9caf9b57da4fb6b5635
Atmos Maciel <atmos.mps@gmail.com>
2019-05-28 17:34:59 -0300

    Refatorando POST de usuarios para o teste passar.

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -2,6 +2,7 @@
 
 from flask import Blueprint, request
 from flask_restful import Resource, Api
+from sqlalchemy import exc
 from project import db
 from project.api.models import User
 
@@ -22,17 +23,33 @@ class UsersList(Resource):
 
     def post(self):
         post_data = request.get_json()
-        username = post_data.get('username')
-        email = post_data.get('email')
-        db.session.add(User(username=username, email=email))
-        db.session.commit()
 
         response_object = {
-            'status': 'success',
-            'message': f'{email} was added!'
+            'status': 'Fail',
+            'message': 'Invalid Payload.'
         }
 
-        return response_object, 201
+        if not post_data:
+            return response_object, 400
+
+        username = post_data.get('username')
+        email = post_data.get('email')
+
+        try:
+            user = User.query.filter_by(email=email).first()
+
+            if not user:
+                db.session.add(User(username=username, email=email))
+                db.session.commit()
+                response_object['status'] = 'success'
+                response_object['message'] = f'{email} was added!'
+                return response_object, 201
+            else:
+                response_object['message'] = 'Sorry. That email already exists.'
+                return response_object, 400
+        except exc.IntegrityError:
+            db.session.rollback()
+            return response_object, 400
 
 
 api.add_resource(UsersList, '/users')
