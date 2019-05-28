f3e1ec3e49636314738adcc3e3790ffca64ca416
Atmos Maciel <atmos.mps@gmail.com>
2019-05-28 15:27:20 -0300

    Adicionando test para verificar se as chaves do Json estão validas ou inválidas.

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -17,6 +17,7 @@ class TestUserService(BaseTestCase):
         self.assertIn('success', data['status'])
 
     def test_add_user(self):
+        """Ensure a new user can be added to the database."""
         with self.client:
             # construinfo uma resposta HTTP para realizar o teste.
             response = self.client.post(
@@ -34,6 +35,7 @@ class TestUserService(BaseTestCase):
             self.assertIn('success', data['status'])
 
     def test_add_user_invalid_json(self):
+        """Ensure error is thrown if the JSON object is empty."""
         with self.client:
             response = self.client.post(
                 '/users',
@@ -46,6 +48,22 @@ class TestUserService(BaseTestCase):
             self.assertIn('Invalid Payload.', data['message'])
             self.assertIn('Fail', data['status'])
 
+    def test_add_user_invalid_json_keys(self):
+        """Ensure error is thrown if the JSON object does not have a username key."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'email': 'atmos@email.com'
+                }),
+                content_type='application/json',
+            )
 
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid Paylod', data['message'])
+            self.assertIn('Fail', data['status'])
+
+    
 if __name__ == '__main__':
     unittest.main()
