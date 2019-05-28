e57eb20243f1d6460e2d13ca2f95c9a694bd85c5
Atmos Maciel <atmos.mps@gmail.com>
2019-05-28 11:30:14 -0300

    Adicionando teste de usuario.

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -16,6 +16,23 @@ class TestUserService(BaseTestCase):
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
 
+    def test_add_user(self):
+        with self.client:
+            # construinfo uma resposta HTTP para realizar o teste.
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'atmos',
+                    'email': 'atmos@email.com'
+                }),
+                content_type='application/json'
+            )
+
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('atmos@email.com was added!', data['message'])
+            self.assertIn('success', data['status'])
+
 
 if __name__ == '__main__':
     unittest.main()
